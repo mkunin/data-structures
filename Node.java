73635807b530c288261616eadb91babb9493af4b
Matthew J. Kunin <matthew.kunin@gmail.com>
2016-05-19 08:32:04 -0500

    edited Node.java

--- Node.java
+++ Node.java
@@ -7,16 +7,13 @@
 class Node {
 	Node next = null;
 	int data, size = 1;
-	String str = "";
 	
 	public Node(int d) {
 		data = d;
-		str += String.valueOf(d);
 	}
 	
 	// Reverses a singly linked list.
 	void reverse() {
-		str = flip(str);
 		Node node = this;
 		Node temp = this;
 		String strWithCommas = "";
@@ -27,12 +24,20 @@ class Node {
 		strWithCommas += String.valueOf(temp.data);
 		strWithCommas = flip(strWithCommas);
 		String[] array = strWithCommas.split(",");
+		flipNumsIn(array);
 		node.data = Integer.valueOf(array[0]);
 		node.next = null;
 		int tempSize = size;
 		size = 1;
 		for (int i = 1; i < tempSize; i++) {
 			node.appendToTail(Integer.valueOf(array[i]));
+		}
+	}
+	
+	// Applies flip(String s) to each item in array.
+	void flipNumsIn (String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = flip(array[i]);
 		}
 	}
 	
@@ -52,23 +57,26 @@ class Node {
 			node = node.next;
 		}
 		node.next = new Node(d);
-		str += String.valueOf(d);
 		size++;
 	}
 	
 	// Prepends a node to the head of a singly linked list.
 	void prependToHead(int d) {
 		Node node = this;
+		Node temp = this;
+		String strWithCommas = "";
+		while (temp.next != null) {
+			strWithCommas += temp.data + ",";
+			temp = temp.next;
+		}
+		strWithCommas += String.valueOf(temp.data);
+		String[] array = strWithCommas.split(",");
 		node.next = null;
 		node.data = d;
-		str += String.valueOf(d);
-		int n = size;
 		size = 1;
-		for (int i = 0; i < n; i++) { /* Appends n nodes to the tail of a singly linked list. */
-			node.appendToTail(Character.getNumericValue(str.charAt(i)));
+		for (int i = 0; i < array.length; i++) { /* Appends n nodes to the tail of a singly linked list. */
+			node.appendToTail(Integer.parseInt(array[i]));
 		}
-		int newStrLen = str.length();
-		str = str.substring(newStrLen-size, newStrLen);
 	}
 	
 	// Removes a node from a singly linked list.
@@ -77,16 +85,12 @@ class Node {
 		if (node.data == d && size > 1) { /* Moves the head */
 			node.data = node.next.data;
 			node.next = node.next.next;
-			str = str.substring(1, size);
 			size--;
 			return;
 		}
-		int count = 0;
 		while (node.next != null) {
-			count++;
 			if (node.next.data == d) { /* Removes  inner node */
 				node.next = node.next.next;
-				str = str.substring(0, count) + str.substring(count+1, size);
 				size--;
 				return;
 			}
